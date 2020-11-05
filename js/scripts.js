acba094d887134d7476965dcc5ea939d79d487fe
jorge abascal lopez <jorgeabascal20@gmail.com>
2020-11-04 20:32:30 -0500

    Finalizando proyecto

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    //Obtener la imagen 
+    const imagenHero = document.querySelector(".hero");
+
+    // Inicializar las variables
+    let i = 0;
+    let tiempo = 0;
+
+    //Arreglo con imagenes de fondo
+    const imagenes = ["arriba2.jpg", "arriba.jpg"];
+
+    //Funcion iontervalo para mover la imagen
+    setInterval( () => {
+        imagenHero.style.backgroundPositionX = `-${tiempo}px`;
+
+        if(tiempo > 30){
+            tiempo = 0;
+            imagenHero.style.backgroundImage = `url(../img/${imagenes[i]})`;
+            
+            if(i === imagenes.length -1){
+                i = 0;
+            }else{
+                i++;
+            }
+
+        }else{
+            tiempo++;
+        }
+    }, 100);
+
     //Boton Flotante en el footer
 
     const btnFlotante = document.querySelector(".btn-flotante");
